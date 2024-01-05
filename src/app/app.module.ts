ae314deb9d014061f53b6d1bae1b8d5ff04739b4
amarua123 <amar.sarkar@accolitedigital.com>
2024-01-05 13:33:12 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
-
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 @NgModule({
   declarations: [
     AppComponent,
     TimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
